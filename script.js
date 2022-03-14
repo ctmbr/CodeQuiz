8fd3e4ead9babb67f992debdf00ddf8ef48c7a68
Charles K.S <ctmbr@protonmail.com>
2022-03-14 10:46:51 -0700

    Adding Question Functionality

--- script.js
+++ script.js
@@ -5,23 +5,81 @@ var questionEl = document.querySelector("#question-list")
 var scoreEl = document.querySelector("#score")
 var timer
 var secondsRemaining = 10
-function startGame() {
+var currentQuestion = 0
+var questions = [
+    {
+        questionText: 'This is the first question',
+        choices: ['A', 'B', 'C', "D"],
+        correctAnswer: 'B'
+    },
+    {
+        questionText: 'This is the second question',
+        choices: ['A', 'B', 'C', "D"],
+        correctAnswer: 'D'
+    },
+    {
+        questionText: 'This is the third question',
+        choices: ['A', 'B', 'C', "D"],
+        correctAnswer: 'A'
+    },
+    {
+        questionText: 'This is the fourth question',
+        choices: ['A', 'B', 'C', "D"],
+        correctAnswer: 'C'
+    },
+
+]
+
+function startGame(e) {
+    console.log(e.target)
     startButton.classList.add("hidden")
     timerEl.innerText = secondsRemaining;
     timerEl.classList.remove("hidden")
     questionEl.classList.remove("hidden")
-    console.log("Hello World")
     scoreEl.innerText = "Score"
     timer = setInterval(function () {
         secondsRemaining--;
         timerEl.innerText = secondsRemaining;
         if (secondsRemaining <= 0) { endGame() }
     }, 1000)
+    questionDisplay()
+}
 
+function questionDisplay() {
+    questionEl.innerHTML = ''
+    var questionText = document.createElement("h3")
+    questionText.innerText = questions[currentQuestion].questionText
+    questionEl.append(questionText)
+
+    var orderedList = document.createElement("ol")
+
+    questions[currentQuestion].choices.forEach(function (choice) {
+        var choiceLi = document.createElement("li")
+        // choiceLi.innerText = choice
+        choiceLi.innerHTML = `<button onclick="selectAnswer(this)">${choice}</button>`
+        orderedList.append(choiceLi)
+    })
+
+    questionEl.append(orderedList)
+}
+
+function selectAnswer(button) {
+    console.log(button.innerText)
+
+    if (button.innerText === questions[currentQuestion].correctAnswer) {
+        console.log('correct')
+    } else (console.log('incorrect'))
+
+    currentQuestion++
+
+    if (currentQuestion >= questions.length) {
+        endGame()
+    } else questionDisplay()
 }
 
 function endGame() {
     clearInterval(timer);
     timerEl.classList.add("game-over")
     timerEl.innerText = "GAME OVER!!"
+    questionEl.classList.add("hidden")
 }
